7fa3ac2ddc7ebc608be49bad3b3fd6ece01ddabc
Arnautu Stefan <arnautu.stefan93@gmail.com>
2019-03-26 14:58:43 +0000

    updated calibration file

--- calibrate.py
+++ calibrate.py
@@ -38,7 +38,7 @@ if __name__ == '__main__':
     args.setdefault('--threads', 4)
     args.setdefault('--camera', 1)
     if not img_mask:
-        img_mask = '../data/left??.jpg'  # default
+        img_mask = './chessboard/*.png'  # default
     else:
         img_mask = img_mask[0]
 
@@ -48,7 +48,7 @@ if __name__ == '__main__':
         os.mkdir(debug_dir)
     square_size = float(args.get('--square_size'))
 
-    pattern_size = (9, 6)
+    pattern_size = (11, 7)
     pattern_points = np.zeros((np.prod(pattern_size), 3), np.float32)
     pattern_points[:, :2] = np.indices(pattern_size).T.reshape(-1, 2)
     pattern_points *= square_size
@@ -56,99 +56,72 @@ if __name__ == '__main__':
     obj_points = []
     img_points = []
     name = 0
-    if args.get('--camera'):
+    exitFlag = 0
+    if args.get('--camera') == 1:
         cap = cv.VideoCapture(0)
-        while(True):
+        while(exitFlag <= 0):
            ret,img = cap.read()
-           img = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+           gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
             
-           found, corners = cv.findChessboardCorners(img, pattern_size)
+           if ret:
+              found, corners = cv.findChessboardCorners(gray, pattern_size)
+              [h, w] = gray.shape[:2]
+
            if found:
                term = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_COUNT, 30, 0.1)
-               cv.cornerSubPix(img, corners, (5, 5), (-1, -1), term)
-               vis = cv.cvtColor(img, cv.COLOR_GRAY2BGR)
-               cv.drawChessboardCorners(vis, pattern_size, corners, found)
+               cv.cornerSubPix(gray, corners, (5, 5), (-1, -1), term)
                name = name + 1
                outfile = os.path.join(debug_dir, str(name) + '_chess.png')
-               cv.imwrite(outfile, vis)
-           else:
-               vis = img;
+               cv.imwrite(outfile, img)
+               cv.drawChessboardCorners(img, pattern_size, corners, found)
+               img_points.append(corners)
+               obj_points.append(pattern_points)
 
-           cv.imshow('image',vis)
-           cv.waitKey(10)
+           cv.imshow('image',img)
+           exitFlag = cv.waitKey(1)
+        cv.destroyAllWindows()
+        cap.release()
+    else:
+       for fn in img_names if debug_dir else []:
+         print(fn)
+         path, name, ext = splitfn(fn)
+         img_found = os.path.join(debug_dir, name + '_chess.png')
+         outfile = os.path.join(debug_dir, name + '_undistorted.png')
 
-    else:    
-        h, w = cv.imread(img_names[0], cv.IMREAD_GRAYSCALE).shape[:2]  # TODO: use imquery call to retrieve results
-
-        def processImage(fn):
-            print('processing %s... ' % fn)
-            img = cv.imread(fn, 0)
-            if img is None:
-                print("Failed to load", fn)
-                return None
-
-            assert w == img.shape[1] and h == img.shape[0], ("size: %d x %d ... " % (img.shape[1], img.shape[0]))
-            found, corners = cv.findChessboardCorners(img, pattern_size)
+         img = cv.imread(fn)
+         if img is None:
+            print("skipped image ",name)
+            continue
+         else:
+            gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+            found, corners = cv.findChessboardCorners(gray,pattern_size)
+            [h, w] = gray.shape[:2]
             if found:
                 term = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_COUNT, 30, 0.1)
-                cv.cornerSubPix(img, corners, (5, 5), (-1, -1), term)
+                cv.cornerSubPix(gray, corners, (5, 5), (-1, -1), term)
+                img_points.append(corners)
+                obj_points.append(pattern_points)
 
-            if debug_dir:
-                vis = cv.cvtColor(img, cv.COLOR_GRAY2BGR)
-                cv.drawChessboardCorners(vis, pattern_size, corners, found)
-                _path, name, _ext = splitfn(fn)
-                outfile = os.path.join(debug_dir, name + '_chess.png')
-                cv.imwrite(outfile, vis)
-
-            if not found:
-                print('chessboard not found')
-                return None
-
-            print('           %s... OK' % fn)
-            return (corners.reshape(-1, 2), pattern_points)
-
-        threads_num = int(args.get('--threads'))
-        if threads_num <= 1:
-            chessboards = [processImage(fn) for fn in img_names]
-        else:
-            print("Run with %d threads..." % threads_num)
-            from multiprocessing.dummy import Pool as ThreadPool
-            pool = ThreadPool(threads_num)
-            chessboards = pool.map(processImage, img_names)
-
-    chessboards = [x for x in chessboards if x is not None]
-    for (corners, pattern_points) in chessboards:
-        img_points.append(corners)
-        obj_points.append(pattern_points)
-
+    print("Exited loop with ",exitFlag)
     # calculate camera distortion
-    rms, camera_matrix, dist_coefs, rvecs, tvecs = cv.calibrateCamera(obj_points, img_points, (w, h), None, None)
+    if obj_points:
+        print("Processing image points\n")
+        rms, camera_matrix, dist_coefs, rvecs, tvecs = cv.calibrateCamera(obj_points, img_points, (w, h), None, None)
 
-    print("\nRMS:", rms)
-    print("camera matrix:\n", camera_matrix)
-    print("distortion coefficients: ", dist_coefs.ravel())
-
-    # undistort the image with the calibration
-    print('')
-    for fn in img_names if debug_dir else []:
-        path, name, ext = splitfn(fn)
-        img_found = os.path.join(debug_dir, name + '_chess.png')
-        outfile = os.path.join(debug_dir, name + '_undistorted.png')
-
-        img = cv.imread(img_found)
-        if img is None:
-            continue
-
-        h, w = img.shape[:2]
-        newcameramtx, roi = cv.getOptimalNewCameraMatrix(camera_matrix, dist_coefs, (w, h), 1, (w, h))
-
-        dst = cv.undistort(img, camera_matrix, dist_coefs, None, newcameramtx)
-
-        # crop and save the image
-        x, y, w, h = roi
-        dst = dst[y:y+h, x:x+w]
-
-        print('Undistorted image written to: %s' % outfile)
-        cv.imwrite(outfile, dst)
-
+        print("\nRMS:", rms)
+        print("camera matrix:\n", camera_matrix)
+        print("distortion coefficients: ", dist_coefs.ravel())
+        
+    cap = cv.VideoCapture(0)
+    exitFlag = 0
+    while(exitFlag <= 0):
+        ret,img = cap.read()
+            
+        if ret:
+            dst = cv.undistort(img, camera_matrix, dist_coefs)
+            cv.imshow('original',img)
+            cv.imshow('undistorted',dst)
+            exitFlag = cv.waitKey(1)
+       
     cv.destroyAllWindows()
+    cap.release()
